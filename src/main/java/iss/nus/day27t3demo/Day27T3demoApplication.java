43c4cf9db14f8570e73ecd5705774043d261275f
yupokok <kimberlywsp@gmail.com>
2024-01-18 09:25:46 +0800

    initial commit

---
+++ src/main/java/iss/nus/day27t3demo/Day27T3demoApplication.java
@@ -0,0 +1,13 @@
+package iss.nus.day27t3demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day27T3demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day27T3demoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iss/nus/day27t3demo/Day27T3demoApplicationTests.java
@@ -0,0 +1,13 @@
+package iss.nus.day27t3demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day27T3demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
